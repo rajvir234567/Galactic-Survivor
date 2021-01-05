9022ea1eb995ed8deb3e4e39f0f18429eb3a3cc2
rajvir234567 <73382875+rajvir234567@users.noreply.github.com>
2021-01-05 12:28:20 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,17 +1,34 @@
-var canvas, backgroundImage,space;
+var canvas, backgroundImage,space,map,neon,stars,test;
 var gameState = "start";
 var form;
-var playerName;
-var neon;
+var playerName,player,playerImage,attack,blue,green,orange,red;
+var mapSelect = 1;
 
 function preload(){
   backgroundImage = loadImage("images/Background1.jpg");
   space = loadImage("images/Background2.jpg");
+  test = loadImage("images/Testing.jpg");
+  stars = loadImage("images/Background4.jpg");
+  playerImage = loadImage("images/Player.png");
+  //blue = loadImage("images/blue.png");
+  //green = loadImage("images/green.png");
+  //orange = loadImage("images/orange.png");
+  //red = loadImage("images/red.png");
 }
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
   form = new Form();
-  neon = createSprite(width/2,height/2);
+  map = createSprite(width/2,height/2);
+  player = createSprite(50,height/2,50,50);
+  player.addImage(playerImage);
+  player.scale= 0.3;
+  attack = createSprite(player.x,player.y,80,5);
+  attack.visible = false;
+  
+  if(mapSelect === 1){
+    map.addImage(stars);
+  }
+
 }
 
 function draw(){
@@ -23,8 +40,47 @@ function draw(){
   }
   if(gameState ===  "map"){
     form.displayMapScreen();
+  
+    if(mapSelect === 2){
+      map.addImage(test);
+    }
+  
   }
   if(gameState === "play"){
+    form.displayPlayScreen();
+    
+    map.velocityX = -4;
+    if(map.x < 150){
+      map.x = width/2;
+    }
+    player.y = mouseY;
+
+    if(keyDown("space")){
+      attack.x = player.x + 50;
+      attack.y = attack.y;
+      attack.visible = true;
+      var rand = Math.round(random(1,4));
+      attack.shapeColor = rgb(random(0,255),random(0,255),random(0,255));
+/*       switch (rand) {
+        case 1:
+          attack.addImage(blue);
+          break;
+        case 2:
+          attack.addImage(green);
+          break;
+        case 3:
+          attack.addImage(orange);
+          break;
+        case 4:
+          attack.addImage(red);
+          break;
+
+        default:
+          break;
+      } */
+      attack.velocityX = 6;
+    }
+
     drawSprites();
   }
 }
